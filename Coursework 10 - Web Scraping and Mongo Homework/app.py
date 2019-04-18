75ec240db318c0050b40a0f40d4fcac530c3150d
Zeyu Yan <codingpractice0723@gmail.com>
2019-04-18 17:55:07 -0500

    updated again

--- Coursework 10 - Web Scraping and Mongo Homework/app.py
+++ Coursework 10 - Web Scraping and Mongo Homework/app.py
@@ -6,20 +6,32 @@ Created on Sat Apr 13 19:37:59 2019
 @author: zeyuyan
 """
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scrape_mars
 
 
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
 
 @app.route("/")
 def index():
+    
     mars_info = mongo.db.mars_info.find_one()
-    return render_template("index.html", mars_info=mars_info)
+    
+    if not mars_info:
+        return redirect(url_for("scraper"))
+    
+    with open("table.html", "r") as in_file:
+        table = in_file.read()
+
+    return render_template(
+            "index.html",
+            mars_info=mars_info,
+            table=table
+    )
     
 
 @app.route("/scrape")
@@ -27,7 +39,7 @@ def scraper():
     mars_info = mongo.db.mars_info
     mars_info_scraped = scrape_mars.scrape_info()
     mars_info.update({}, mars_info_scraped, upsert=True)
-    return redirect("/", code=302)
+    return redirect(url_for("index"), code=302)
 
 
 if __name__ == "__main__":
